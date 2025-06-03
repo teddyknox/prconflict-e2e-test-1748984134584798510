9f9671b2aa9faf6cfda0ccdaeb60238539db6c6f
GitHub CI <ci@github.com>
2025-06-03 20:55:35 +0000

    Add initial test files

---
+++ edge.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println("line 4")
+	fmt.Println("line 5") 
+	fmt.Println("line 6")
+	fmt.Println("line 7")
+	fmt.Println("line 8")
+	fmt.Println("line 9")
+	fmt.Println("line 10")
+}
